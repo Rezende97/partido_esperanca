0230be9bdfd34ace360218917c41002a51eef9cd
rezende97 <julianomaciel04@gmail.com>
2025-03-16 10:02:20 -0300

    first commit

---
+++ src/app/components/layouts/Header.jsx
@@ -0,0 +1,177 @@
+"use client";
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from '@mui/material/Stack';
+
+import Image from "next/image";
+import logoEsperanca from '@/app/images/logoEsperanca.png'
+
+const pages = ["Partido", "História", "Estatuto", "Programa", "Código de Ética", "Diretório Nacional", "Diretório Estadual"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [selectedPage, setSelectedPage] = React.useState(pages[0]);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <AppBar position="fixed" sx={{ background: '#ffffff', top: 0, left: 0, right: 0, zIndex: 1300 }}>
+        <Container maxWidth="xl">
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "1.5rem",
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+              width: '100%'
+            }}
+          >
+            <Image
+              src='/LogoESPERANCA.png'
+              alt="Descrição da imagem"
+              width={150} // Largura da imagem
+              height={150} // Altura da imagem
+              priority // Carrega a imagem prioritariamente
+            />
+          </Typography>
+          <Toolbar disableGutters>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="#1C2961"
+                sx={{
+                  color: "#1C2961"
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "center",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: "block", md: "none" } }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={() => setSelectedPage(page)}>
+                    <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+                justifyContent: "center",
+                gap: '12px'
+              }}
+            >
+              {pages.map((page, index) => (
+                <Button
+                  key={page}
+                  onClick={() => setSelectedPage(page)}
+                  sx={{
+                    my: 2,
+                    color: selectedPage === page ? "#A9C705" : "#1C2961",
+                    display: "block",
+                    borderBottom: selectedPage === page ? "2px solid #A9C705" : "none",
+                  }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            <Stack direction="row" spacing={2}>
+              <Button variant="contained">Ficha de apoio</Button>  
+              <Button variant="contained" color="success">Doe agora!</Button>
+            </Stack>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}></IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography sx={{ textAlign: "center" }}>{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      {/* Adicionando padding-top para evitar que o conteúdo fique por baixo do menu fixo */}
+      <Box sx={{ paddingTop: '120px' }}>
+        {/* Aqui vai o conteúdo da página */}
+      </Box>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/app/homePage/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Header from "../components/layouts/Header";
+
+export default function HomePage(){
+    return(
+        <>
+            <Header />
+        </>
+    )
+}
